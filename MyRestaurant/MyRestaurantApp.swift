c665465d46a99e8554c4e2aea8159ce3d32b9781
Laszlo Gyurkovics <gyurkovics95@gmail.com>
2024-05-22 00:32:45 +0200

    Initial Commit

---
+++ MyRestaurant/MyRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRestaurantApp.swift
+//  MyRestaurant
+//
+//  Created by Gyurkovics László on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
